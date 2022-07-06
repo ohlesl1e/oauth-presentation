52d5868498b2d6d3fa3e223be07d207915d323a7
ohlesl1e <ohlesl1e420@gmail.com>
2022-07-06 15:52:03 -0700

    extracted title

--- src/pages/Title.svelte
+++ src/pages/Title.svelte
@@ -1,37 +1,22 @@
 <div class="flex flex-col min-h-screen justify-center">
     <div
-        class="i-openmoji-gem-stone animate__animated animate__flip text-9xl max-w-5/6 mx-auto my-8 text-blue-600"
+        class="i-logos-oauth animate__animated animate__flip text-9xl max-w-5/6 mx-auto my-8 text-blue-600"
     />
     <h1
         class="font-sans font-black text-neutral-900 text-center text-7xl tracking-tighter max-w-5/6 mx-auto my-8 leading-none"
     >
-        Svelte-Slides is a HTML presentation tool.
+        OAuth 2.0 and OpenID Connect
     </h1>
-    <p
-        class="font-sans font-normal text-center text-lg text-gray-500 max-w-4/6 mx-auto mt-8 leading-relaxed"
+    <h2
+        class="font-sans font-normal text-center text-5xl text-gray-500 max-w-4/6 mx-auto mt-8 leading-relaxed"
     >
-        Just a collection of slides created with <a
-            href="https://svelte.dev/"
-            class="text-blue-600 hover:text-neutral-600">Svelte</a
-        >
-        and
-        <a
-            href="https://unocss.antfu.me/"
-            class="text-blue-600 hover:text-neutral-600">UnoCSS</a
-        >.
-        <br />
-        Life is too short to fight against Powerpoint or Beamer ugly style!
-    </p>
-    <a
-        href="https://github.com/AlexandreKempf/svelte-slides"
-        class="font-sans text-center text-lg max-w-4/6 mx-auto mt-8 leading-relaxed font-semibold text-blue-600 hover:text-neutral-600"
-        > Check the github <span class="i-akar-icons-github-fill -mt-0.5"/>
-    </a>
+        Leslie Zhou
+    </h2>
 
     <p
         class="font-sans font-normal text-center text-lg text-gray-300 max-w-4/6 mx-auto mt-8 leading-relaxed"
     >
-        Press the right-left key to navigate or tap the right-left side
-        of the screen
+        Press the right-left key to navigate or tap the right-left side of the
+        screen
     </p>
 </div>
